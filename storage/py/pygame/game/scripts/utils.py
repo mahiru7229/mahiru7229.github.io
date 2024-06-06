7f0c3f8fc6cb4e6342b760f3478d0a70148c7ebf
Huỳnh Ngọc Hoàng Tùng <110292918+mahiru7229@users.noreply.github.com>
2024-06-06 22:21:47 +0700

    update download center
    + game.rar

---
+++ storage/py/pygame/game/scripts/utils.py
@@ -0,0 +1,58 @@
+import pygame
+import os
+
+BASE_IMG_PATH = 'data/images/'
+
+
+def load_image(path): # load 1 hinh anh
+    """Load Image (1 Image)
+
+    Args:
+        path (str): Path (Start from data/images/)
+
+    Returns:
+        img: Image
+    """
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img.set_colorkey((0,0,0))
+    return img
+
+
+def load_images(path):
+    """Load Images (>= 2 Images)
+
+    Args:
+        path (str): Path (Start from data/images/)
+
+    Returns:
+        images: Images
+    """
+    images = []
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
+        images.append(load_image(path + '/' + img_name))
+    return images
+    
+    
+    
+class Animation:
+    def __init__(self,images,img_dur = 5, loop = True):
+        self.images = images
+        self.loop = loop
+        self.img_duration = img_dur
+        self.done = False
+        self.frame = 0
+        
+    def copy(self):
+        return Animation(self.images,self.img_duration,self.loop)
+    
+    def update(self):
+        if self.loop:
+            self.frame  = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame  = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
+            
+    
+    def img(self):
+        return self.images[int(self.frame / self.img_duration)]
